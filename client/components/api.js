06d8377c12d64faa21b30d99f9fa6d100ca58152
Marina Tateoka <marinaslussarek@gmail.com>
2019-11-24 22:34:23 +1300

    create api file

---
+++ client/components/api.js
@@ -0,0 +1,9 @@
+import request from 'superagent'
+
+const campsUrl = 'http://localhost:3000/api/v1/camps/'
+
+export function getCampList () {
+  return request
+    .get(campsUrl)
+    .then(response => response.body)
+}
